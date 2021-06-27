c10e36b248c480fbd861013bdcb7be354a20295e
Litu Zou <litu.zou@outlook.com>
2021-06-27 10:02:39 +0800

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,68 @@
+﻿using System.IO;
+using Serilog;
+using System.Threading;
+using System.Drawing.Imaging;
+using Microsoft.Extensions.Configuration;
+using System;
+
+namespace StealthScreenCapture
+{
+    class Program
+    {
+        static int CaptureInterval;
+        static ImageCodecInfo Codec;
+        static EncoderParameters ImageEncoderParameters;
+        static void loadLogger()
+        {
+            string logFile = Path.Join(Directory.GetCurrentDirectory(), "logs.txt");
+            Log.Logger = new LoggerConfiguration()
+                .MinimumLevel.Information()
+                .WriteTo.Console()
+                .WriteTo.File(logFile)
+                .CreateLogger();
+            Log.Debug("Logger is now configured");
+        }
+
+        static void loadConfig()
+        {
+            var config = new ConfigurationBuilder()
+                .SetBasePath(Directory.GetCurrentDirectory())
+                .AddJsonFile("appsettings.json")
+                .Build();
+            CaptureInterval = config.GetValue<int>("SSC:CaptureInterval");
+            long imageQuality = config.GetValue<long>("SSC:ImageQuality");
+            Log.Information($"Capture Interval: {CaptureInterval}s, Image Quality: {imageQuality}");
+            ImageFormat format = ImageFormat.Jpeg;
+            Codec = Array.Find(ImageCodecInfo.GetImageEncoders(), encoder => encoder.FormatID == format.Guid);
+            ImageEncoderParameters = new EncoderParameters(1);
+            ImageEncoderParameters.Param[0] = new EncoderParameter(Encoder.Quality, imageQuality);
+        }
+
+        static bool stopCondition()
+        {
+            string stopFile = Path.Join(Directory.GetCurrentDirectory(), "stop.txt");
+            if (File.Exists(stopFile))
+            {
+                Log.Information($"Stop condition is triggered");
+                File.Delete(stopFile);
+                return true;
+            }
+            return false;
+        }
+
+        static void Main(string[] args)
+        {
+            loadLogger();
+            loadConfig();
+            while (!stopCondition())
+            {
+                using (var bitmap = ScreenCaptureUtils.CaptureScreen())
+                {
+                    ScreenCaptureUtils.SaveImageWithDatetime(bitmap, Codec, ImageEncoderParameters);
+                }
+                Thread.Sleep(CaptureInterval * 1000);
+            }
+            
+        }
+    }
+}
---
+++ ScreenCaptureUtils.cs
@@ -0,0 +1,45 @@
+﻿using Serilog;
+using System;
+using System.Drawing;
+using System.Drawing.Imaging;
+using System.IO;
+using System.Windows.Forms;
+
+namespace StealthScreenCapture
+{
+    static class ScreenCaptureUtils
+    {
+        public static Bitmap CaptureScreen()
+        {
+            int width = Screen.PrimaryScreen.Bounds.Width;
+            int height = Screen.PrimaryScreen.Bounds.Height;
+            Bitmap memoryImage = new Bitmap(width, height);
+            Size s = new Size(width, height);
+            using (Graphics graphics = Graphics.FromImage(memoryImage))
+            {
+                graphics.CopyFromScreen(0, 0, 0, 0, s, CopyPixelOperation.SourceCopy);
+            }
+            return memoryImage;
+        }
+
+        public static void SaveImageWithDatetime(Bitmap image, ImageCodecInfo imageCodecInfo, EncoderParameters encoderParameters)
+        {
+            string path = Path.Join(Directory.GetCurrentDirectory(), "screenshot", DateTime.Now.ToString("yyyy-MM-dd"));
+            string filename = DateTime.Now.ToString("yyyy-MM-dd_HH-mm-ss") + ".jpg";
+            try
+            {
+                if (!Directory.Exists(path))
+                {
+                    Log.Information($"Directory {path} does not exists. Attempting to create one...");
+                    Directory.CreateDirectory(path);
+                }
+                image.Save(Path.Join(path, filename), imageCodecInfo, encoderParameters);
+                Log.Debug($"Image {filename} is created successfully");
+            }
+            catch (Exception e)
+            {
+                Log.Error(e.Message);
+            }
+        }
+    }
+}
